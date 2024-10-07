5268eb97eeff833f90097fb08d3980b40b134aae
Arxonait <nikk23716@gmail.com>
2024-10-07 12:53:38 +0300

    change: func converter

--- main/func_converter.py
+++ main/func_converter.py
@@ -2,31 +2,30 @@ import inspect
 from typing import NamedTuple
 
 
-class FuncData(NamedTuple):
-    func_name: str
-    func_args: dict[str, str]
-    return_type: str = "Any"
+class FuncData:
+    def __init__(self, func):
+        self.func = func
+
+        self.func_args = {}
+        self.name_func: str = None
+        self.return_annotation = "Any"
+        self.__parse_func_data()
+
+    def __parse_func_data(self):
+        self.name_func = self.func.__name__
+        # Сигнатура функции с аргументами
+        func_signature = inspect.signature(self.func)
+        # Список аргументов
+        self.func_args = func_signature.parameters
+
+        # Получаем аннотацию типа возвращаемого значения
+        return_annotation = func_signature.return_annotation
+        if return_annotation is not inspect.Signature.empty:
+            self.return_annotation = str(return_annotation)
+
+    def __create_func_head_from_dict(self) -> str:
+        args_func = ", ".join(map(str, self.func_args.values()))
+        return f"def {self.name_func}({args_func}) -> {self.return_annotation}:"
 
 
-def convert_func_to_dict(func) -> FuncData:
-    name_func = func.__name__
-    # Сигнатура функции с аргументами
-    func_signature = inspect.signature(func)
-    # Список аргументов
-    func_args = func_signature.parameters
-    # Получаем аннотацию типа возвращаемого значения
-    return_annotation = func_signature.return_annotation
-    func_data = {
-        "func_name": name_func,
-        "func_args": func_args
-    }
-
-    if return_annotation is not inspect.Signature.empty:
-        func_data["return_type"] = str(return_annotation)
-    return FuncData(**func_data)
-
-
-def create_func_head_from_dict(func_data: FuncData) -> str:
-    args_func = ", ".join(map(str, func_data.func_args.values()))
-    return f"def {func_data.func_name}({args_func}) -> {func_data.return_type}:"
 

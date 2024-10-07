60149fc9ea5e1ed066c5ec48cab67fea415e6a59
Arxonait <nikk23716@gmail.com>
2024-10-07 19:48:56 +0300

    change: task.py

--- main/task.py
+++ main/task.py
@@ -1,16 +1,27 @@
 import datetime
 import uuid
 import json
+from dataclasses import dataclass
 from typing import Any
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Task(BaseModel):
     name_func: str
     func_args: list
     func_kwargs: dict
-    result: Any = None
 
-    task_id: uuid.UUID = uuid.uuid4()
-    date_create_task: datetime.datetime = datetime.datetime.now(datetime.timezone.utc)
+    task_id: uuid.UUID = Field(default_factory=uuid.uuid4)
+    date_create_task: datetime.datetime = Field(default_factory=datetime.datetime.utcnow)
+
+
+class TaskDone(Task):
+    result: Any = None
+    exception_info: str | None = None
+    date_done_task: datetime.datetime = datetime.datetime.now(datetime.timezone.utc)
+
+
+def task_to_task_done(task: Task, exception_info=None, result=None):
+    task = TaskDone(**task.model_dump(), exception_info=exception_info, result=result)
+    return task

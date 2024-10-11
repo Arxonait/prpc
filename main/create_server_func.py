dd8880a9d1315c9dd101a179593437b82e51eebd
Arxonait <nikk23716@gmail.com>
2024-10-11 08:24:53 +0300

    change: create_server_func.py

--- main/create_server_func.py
+++ main/create_server_func.py
@@ -6,17 +6,19 @@ from main.app_client import get_function_server
 current_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(current_dir, "server_func.py")
 
-lines = [
+head_lines = [
         "from main.app_client import AwaitableTask",
-        "from typing import Any"
     ]
+head_lines = list(map(lambda item: item + " \n", head_lines))
+lines = [*head_lines, "\n\n"]
 
 awaitable = get_function_server()
 awaitable.wait_result_task(datetime.timedelta(seconds=30))
 result: list[str] = awaitable.get_result()
 
-lines.extend(result)
-lines = list(map(lambda item: item + " \n", lines))
+body_lines = result
+body_lines = list(map(lambda item: item + " \n\n", body_lines))
+lines.extend(body_lines)
 
 with open(file_path, mode="w") as file:
     file.writelines(lines)
